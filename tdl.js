9eafc210c943312de644efb2d044bddc08e974e4
paul <paul.j.ukken@gmail.com>
2025-10-01 17:05:59 +0530

    3rd commit

--- tdl.js
+++ tdl.js
@@ -14,11 +14,36 @@ function addtask()
     <input type="checkbox">
     <span>${task}</span>
   </label>
-  <span class="edit-btn">Edit</span>
-  <span class="delete-btn">Delete</span>
+  <span class="editb">Edit</span>
+  <span class="deleteb">Delete</span>
 `;
 listcontainer.appendChild(li);
 inputbox.value=""
+
+
    
 }
+const checkbox=li.querySelector("input");
+const editbutton=li.querySelector(".editb");
+const tspan=li.querySelector("span");
+const deletebutton=li.inputbox.querySelector("deleteb");
+
+checkbox.addEventListener("click",function() {
+    li.classList.toggle("Completed",checkbox.checked)
+});
+
+editbutton.addEventListener("click",function(){
+    const update=prompt("Edit task:",taskSpan.textContent);
+    if (update!==null)
+    {
+        taskSpan.textContent=update;
+        li.classList.remove("Completed");
+    }
+
+});
+li.classList.remove("Completed");
+const completedcounter=document.getElementById("ccounter");
+const uncompletedcounter=document.getElementById("ucounter");
+
+
  
